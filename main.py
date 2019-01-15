808231c09d190521bb6a68b0fe872eb1609a3eca
Aymane <aymane.abdali@gmail.com>
2019-01-15 15:36:20 +0100

    annulation commit d avant

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import logging
 import pyttsx3
 import sys
 
+# OpenCV constant initialization
+font = cv2.FONT_HERSHEY_SIMPLEX
+green = (0, 255, 0)
+
 logger = logging.getLogger('Eye For You Webcam')
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
@@ -16,62 +20,70 @@ formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-sys.path.append('\yolo')
-from yolo import yolo as yl
+sys.path.append('./object_detection/')
+# from yolo import yolo
 
 
-############## Text Recognition ################
-
-def Read_Text(image):
-    return (image, [])
-
-
-################################################
-
-############## Obstacle Recognition ############
-
-def Obstacle_Recognition(image):
-    return (image, [])
+def read_text(image):
+    """Read text contained in image and return it in readable format"""
+    return None, None
 
 
-################################################
-
-############## Face Recognition ################
-
-def Face_Recognition(image):
-    return (image, [])
+def obstacle_recognition(image):
+    """Detect obstacles in image and return description"""
+    return None, None
 
 
-def Do_Nothing(image):
-    return (image, [])
+def face_recognition(image):
+    """Detect faces and recognize them, return description"""
+    return None, None
 
 
-################################################
+def nothing(image):
+    return None, None
+
+
 def say(text):
-    '''engine = pyttsx3.init()
-    engine.say("Object Recognized " + text)
-    a=engine.runAndWait()'''
     engine = pyttsx3.init()
-    rate = engine.getProperty('rate')
-    engine.setProperty('rate', rate)
-    voices = engine.getProperty('voices')
-    # for voice in voices:
-    en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
-    engine.setProperty('voice', en_voice_id)
+    # Rate of speak determined via trial and error
+    rate =  120
+    engine.setProperty("rate", rate)
+    engine.setProperty("voice", "french")
     # print voice.id
-    engine.say('Object Recognized' + text)
-    a = engine.runAndWait()  # blocks
+    engine.say(text)
+    a = engine.runAndWait()
+
+
+applications = {
+    "read_text": read_text,
+    "obstacle_recognition": obstacle_recognition,
+    "face_recognition": face_recognition,
+    "nothing": nothing
+}
+
+
+def get_key_pressed():
+    """
+    Return the mode selected depending on the key pressed
+    """
+    if keyboard.is_pressed('a'):
+        return "read_text"
+    elif keyboard.is_pressed('z'):
+        return "obstacle_recognition"
+    elif keyboard.is_pressed('e'):
+        return "face_recognition"
+    elif keyboard.is_pressed('r'):
+        return "nothing"
 
 
 if __name__ == '__main__':
 
-    # All the args argument:
+    # Argument parsing
     parser = argparse.ArgumentParser(description='Main-Code')
-    # parser.add_argument('--video', type=str, default='')
-    parser.add_argument('--camera', type=int, default=0)
-
-    parser.add_argument('--resolution', type=str, default='432x368', help='network input resolution. default=432x368')
-    parser.add_argument('--save_video', type=str, default='', help='Save in a video format')
+    parser.add_argument('--camera', type=bool, default=False, help="True if the webcam should be used")
+    parser.add_argument('--resolution', type=str, default='432x368', help="CNN input resolution. Default: 432x368")
+    parser.add_argument('--save_video', type=bool, default=False, help="Save the video locally")
+    parser.add_argument('--video', type=str, default='')
     args = parser.parse_args()
 
     # cap = cv2.VideoCapture(args.video)
@@ -84,50 +96,27 @@ if __name__ == '__main__':
     logger.info('cam image=%dx%d' % (image.shape[1], image.shape[0]))
     frames_counter = 1
     # Default Mode
-    Mode = 'Do Nothing'
-    saying = False
+    mode = "nothing"
     while True:
-        # Capturing the frame:
-        ret_val, image = cam.read()
-        ################### Inputs #####################
-
-        if keyboard.is_pressed('a'):
-            Mode = 'Read Text'
-        elif keyboard.is_pressed('z'):
-            Mode = 'Obstacle Recognition'
-            saying = True
-        elif keyboard.is_pressed('e'):
-            Mode = 'Face Recognition'
-
-        elif keyboard.is_pressed('r'):
-            Mode = 'Do Nothing'
-        elif keyboard.is_pressed('t'):
-            Mode = 'Obstacle Recognition'
-            saying = True
-        Functions = {'Read Text': Read_Text, 'Obstacle Recognition': yl.yolo, 'Face Recognition': Face_Recognition,
-                     'Do Nothing': Do_Nothing}
+        now = time.time()
+        # Capturing the frame
+        _, image = cam.read()
+        # Get the mode of treatment
+        mode = get_key_pressed()
         if frames_counter % 3 == 0:
-            output, textes = Functions[Mode](image)
-
-            cv2.putText(output, "FPS: %f Mode Detection : %s" % ((1.0 / (time.time() - fps_time)), Mode), (10, 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            output, texts = applications[mode](image)
+            fps = 1.0 / (now - fps_time)
+            # Show capture info on image
+            cv2.putText(output, "FPS: %f Mode Detection : %s" % (fps, mode), (10, 10), font, 0.5, green, 2)
             cv2.imshow('computation result', output)
-            if saying:
-                for text in textes:
-                    say(text)
+            print(texts)
 
+        say("bonjour je suis un ordinateur avec une voix nulle")
         frames_counter += 1
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        # Leave the loop is q is pressed
+        if (cv2.waitKey(1) & 0xFF) == ord('q'):
             break
 
         fps_time = time.time()
     cv2.destroyAllWindows()
-
-################################################
-
-
-################### Outputs ####################
-
-
-################################################
